f28f5c17b80ad185db2dd51c0876ec68a477abd2
Benjamín Naranjo <Ben.Nar.95@gmail.com>
2020-06-08 16:44:06 -0400

    hola2

--- MongoConection.py
+++ MongoConection.py
@@ -1,14 +1,19 @@
 import pymongo
 import pprint
 
-CLIENT = pymongo.MongoClient("mongodb://localhost:27017/")
-DDBB = CLIENT["G5Proyecto"]
-COLLECTION = DDBB["Subvencion"]
 
-
-query = COLLECTION.find_one()
-pprint.pprint(query)
 
 def hola():
     hola = "hola"
     return hola
+
+def main():
+    CLIENT = pymongo.MongoClient("mongodb://localhost:27017/")
+    DDBB = CLIENT["G5Proyecto"]
+    COLLECTION = DDBB["Subvencion"]
+
+    query = COLLECTION.find_one()
+    pprint.pprint(query)
+
+if __name__ == '__main__':
+    main()
