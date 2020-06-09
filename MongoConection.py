4da3042f012b481b50f048e46cec0d9beba087fa
Benjamín Naranjo <Ben.Nar.95@gmail.com>
2020-06-08 21:22:00 -0400

    2da Query

--- MongoConection.py
+++ MongoConection.py
@@ -6,20 +6,32 @@ CLIENT = pymongo.MongoClient("mongodb://localhost:27017/")
 DDBB = CLIENT["G5Proyecto"]
 COLLECTION = DDBB["Subvencion"]
 
-#La primera consulta a realizar será la comparación de la cifra anual, junto a la cifra mensual del monto total subvencionado a zonas rurales vs zonas urbanas.
 def EscolaridadPorArea(anno):
-    pipeline = json.loads(
-        '[{"$group": {"_id": {"mes": "$mes","anno": "%s", "area": "$area"},"totalEscolaridad": {"$sum": "$escolaridad"},"count": {"$sum": 1}}},{"$sort": {"_id.mes": 1}}]' %(anno))
+    pipeline = json.loads('[{"$group": {"_id": {"mes": "$mes","anno": "%s", "area": "$area"},"totalEscolaridad": {"$sum": "$escolaridad"},"count": {"$sum": 1}}},{"$sort": {"_id.mes": 1}}]' %(anno))
     cursor = COLLECTION.aggregate(pipeline)
     listUrbana=[]
     listRural=[]
 
     for element in cursor:
-        pprint.pprint(element)
-
         if(element['_id']['area'] == "Urbano"):
             listUrbana.append(element)
         else:
             listRural.append(element)
-
     return listUrbana, listRural
+
+def EscolaridadPromedioPorArea(anno):
+    pipeline= json.loads('[{"$group": {"_id": {"area": "$area","anno": "%s","mes": "$mes"},"escolaridad": {"$sum": "$escolaridad"},"count": {"$sum": 1}}}'
+                         ', {"$project":{"escolaridadPorInstritucion": {"$divide":["$escolaridad", "$count"]}}},'
+                         '{"$sort": {"_id.mes": 1}}]' %(anno))
+    cursor = COLLECTION.aggregate(pipeline)
+    listUrbana = []
+    listRural = []
+
+    for element in cursor:
+        if (element['_id']['area'] == "Urbano"):
+            listUrbana.append(element)
+        else:
+            listRural.append(element)
+    return listUrbana, listRural
+
+
